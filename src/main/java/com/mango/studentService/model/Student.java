c4200769935851da676bdc4bad10f53493fbdf0a
tomszociepa <tom.ociepa@gmail.com>
2023-08-08 11:38:08 +0200

    add lombok

--- src/main/java/com/mango/studentService/model/Student.java
+++ src/main/java/com/mango/studentService/model/Student.java
@@ -1,6 +1,8 @@
 package com.mango.studentService.model;
 
 import com.fasterxml.jackson.databind.annotation.EnumNaming;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -8,6 +10,8 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import javax.validation.constraints.*;
 
 @Document
+@Getter
+@Setter
 public class Student {
     @Id
     private String id;
@@ -36,43 +40,5 @@ public class Student {
     public Student() {
     }
 
-    public String getId() {
-        return id;
-    }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Status getStatus() {
-        return status;
-    }
-
-    public void setStatus(Status status) {
-        this.status = status;
-    }
 }
